010d0dfbfb6e66cc7e94d0c8a9ac7a2509d8e049
Eron Alves <eron.alves@rocketmail.com>
2022-11-26 11:10:54 -0300

    Configured servlet

---
+++ src/main/java/jspcomponentization/servlet/ComponentServlet.java
@@ -0,0 +1,23 @@
+package jspcomponentization.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/component")
+public class ComponentServlet extends HttpServlet {
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/templates/components.jsp");
+		requestDispatcher.forward(request, response);
+	}
+	
+	
+}
---
+++ src/main/webapp/templates/components.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Components Test</title>
+</head>
+<body>
+	<h2>This is a component test</h2>
+</body>
+</html>
