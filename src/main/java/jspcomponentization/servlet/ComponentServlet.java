fb8426b49a0c92db8783ac5c90ee0f074ce7d852
Eron Alves <eron.alves@rocketmail.com>
2022-11-26 11:34:58 -0300

    Started componentization

--- src/main/java/jspcomponentization/servlet/ComponentServlet.java
+++ src/main/java/jspcomponentization/servlet/ComponentServlet.java
@@ -1,7 +1,8 @@
 package jspcomponentization.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,13 +13,22 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/component")
 public class ComponentServlet extends HttpServlet {
-	
+
+	Map<String, String> components = new HashMap<>();
+
+	@Override
+	public void init() {
+		components.put("shifting", "<p>This is the shifting component</p>" + "<h3>What is this shifting</h3>"
+				+ "<p>Shifting component is just a generic component</p>");
+		components.put("ouroboros",
+				"<p>Ouruboros component</p>" + "<h3 style=\"color:green;\">Ouroboros component is awesome</h3>");
+	}
+
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println(request.getAttribute("callComponent"));
+		request.setAttribute("component", components.get(request.getAttribute("callComponent")));
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/templates/components.jsp");
 		requestDispatcher.forward(request, response);
 	}
-	
-	
+
 }
