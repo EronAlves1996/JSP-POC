00fa1619d43d27f5306793826e7f4befcd328c70
Eron Alves <eron.a.silva@capgemini.com>
2022-11-28 21:26:26 -0300

    Adjusted the filter to deliver the list of branchs for tree router

--- src/main/java/jspcomponentization/servlet/ServletFilter.java
+++ src/main/java/jspcomponentization/servlet/ServletFilter.java
@@ -1,6 +1,8 @@
 package jspcomponentization.servlet;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.FilterChain;
 import javax.servlet.RequestDispatcher;
@@ -14,8 +16,10 @@ import javax.servlet.http.HttpServletResponse;
 public class ServletFilter extends HttpFilter { 	
 	@Override
 	public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException{
-		String[] splitted = request.getRequestURI().split("/");
-		request.setAttribute("component", splitted[splitted.length-1]);
+		List<String> routes = Arrays.asList(request.getRequestURI().split("/"));
+		routes.remove(0);
+		routes.remove(0);
+		request.setAttribute("route", routes);
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/component");
 		requestDispatcher.forward(request, response);
 	}
